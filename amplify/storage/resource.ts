c7984cac2f55a33ddcb69baac9386bdc2b2df29c
philfpvr <phil@focalpointvr.com>
2025-02-10 15:11:22 +0000

    switch to fvpr-test2

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,18 +1,24 @@
 import { defineStorage } from '@aws-amplify/backend';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { RemovalPolicy } from 'aws-cdk-lib';
 
-export const storage = defineStorage({
-  name: 'myS3Bucket',
-  access: (allow) => ({
-    'public/*': [
-      allow.guest.to(['read']),
-      allow.authenticated.to(['read', 'write', 'delete']),
-    ],
-    'protected/{entity_id}/*': [
-      allow.authenticated.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ],
-    'private/{entity_id}/*': [
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
-  })
+export const storage = defineStorage((context: any) => {
+  return {
+    name: 'amp-fpvr-test2',
+    access: (allow: any) => ({
+      '/*': [
+        allow.authenticated().to(['read', 'write', 'delete']),
+      ],
+    }),
+    overrides: (bucketResource: any, ctx: any) => {
+      const existingBucket = s3.Bucket.fromBucketName(
+        bucketResource,
+        'fpvr-test2',
+        'fpvr-test2'
+      );
+
+      bucketResource.s3Bucket = existingBucket as unknown as typeof bucketResource.s3Bucket;
+      existingBucket.applyRemovalPolicy(RemovalPolicy.RETAIN);
+    },
+  };
 });
