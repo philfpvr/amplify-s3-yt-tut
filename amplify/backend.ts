95f7b3f50d37643b690aea2aea4ef154214f6cd9
philfpvr <phil@focalpointvr.com>
2025-02-10 17:06:55 +0000

    return to tut2

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,16 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { auth } from './auth/resource.js';
+import { data } from './data/resource.js';
+import { storage } from './storage/resource';
 
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
-});
-
-
-backend.addOutput({
-  storage: {
-    aws_region: "eu-west-1",
-    bucket_name: "drm-vod-2"
-  },
+  storage
 });
